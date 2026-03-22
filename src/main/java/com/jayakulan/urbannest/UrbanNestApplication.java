77f8e26118227d17659e892cf2683d6146f4ec51
jayakulan <vjayakulan@gmail.com>
2026-03-22 18:27:38 +0530

    create file

---
+++ src/main/java/com/jayakulan/urbannest/UrbanNestApplication.java
@@ -0,0 +1,13 @@
+package com.jayakulan.urbannest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrbanNestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jayakulan/urbannest/UrbanNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayakulan.urbannest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
